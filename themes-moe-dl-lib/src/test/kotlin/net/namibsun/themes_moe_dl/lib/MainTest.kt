0197bb52bc020f32ac218952f28310ee5dbecff7
Hermann Krumrey <hermann@krumreyh.com>
2017-02-09 00:48:19 +0100

    Integrate Test coverage plugin
    Former-commit-id: 5394de9b6875d5331d897896f9001c0e2d47adcc

---
+++ themes-moe-dl-lib/src/test/kotlin/net/namibsun/themes_moe_dl/lib/MainTest.kt
@@ -0,0 +1,13 @@
+package net.namibsun.themes_moe_dl.lib
+
+import org.junit.Test
+import org.junit.Assert.assertTrue
+
+class MainTest {
+
+    @Test
+    fun tester() {
+        assertTrue(true)
+    }
+
+}
