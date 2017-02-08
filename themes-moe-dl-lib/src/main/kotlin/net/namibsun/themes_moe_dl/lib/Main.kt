fe6ab5cc3727a1b78aa51156e2efe168ffa5fad5
Hermann Krumrey <hermann@krumreyh.com>
2017-02-09 00:31:29 +0100

    Implement Documentation generator
    Former-commit-id: 6fbd73e20230146baf17fd08b20c85f5b697c0fe

--- themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/Main.kt
+++ themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/Main.kt
@@ -1,6 +1,13 @@
-
 package net.namibsun.themes_moe_dl.lib
 
-    fun main(args: Array<String>) {
-        println("Hello World")
-    }
+import org.jsoup.Jsoup
+
+/**
+ * @param args The
+ */
+fun main(args: Array<String>) {
+    println("Hello World")
+
+    val result = Jsoup.connect("https://themes.moe").data("username", "namboy94").post()
+    print(result)
+}
