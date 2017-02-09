3370f5691ab622181533218b8e266d012dc72dfb
Hermann Krumrey <hermann@krumreyh.com>
2017-02-09 08:07:13 +0100

    Add classes necessary to model parser

--- themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/parsing/Theme.kt
+++ themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/parsing/Theme.kt
@@ -17,31 +17,3 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with themes.moe-dl.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-import org.junit.Test
-
-/**
- * Unit Testing class that tests the [ThemesMoeParser] class
- */
-class ThemesMoeParserTest {
-    val standardParser = ThemesMoeParser()
-
-    /**
-     * Tests if the parser fetches the list of series for a specific
-     * [myanimelist.net](https://myanimelist.net) user
-     */
-    @Test
-    fun testFetchingMalUserList() {
-        val results = standardParser.fetchUserList("namboy94", MYANIMELIST)
-    }
-
-    /**
-     * Tests if the parser fetches the list of series for a specific
-     * [hummingbird.me](https://hummingbird.me) user
-     */
-    @Test
-    fun testFetchingHummingbirdUserList() {
-        val results = standardParser.fetchUserList("namboy94", HUMMINGBIRD)
-    }
-
-}
