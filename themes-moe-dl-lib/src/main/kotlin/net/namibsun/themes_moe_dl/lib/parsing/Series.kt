c16bb7e578db8ef2e2ea286feda165f7714a94e3
Hermann Krumrey <hermann@krumreyh.com>
2017-02-09 10:58:29 +0100

    Implement basic models of Series and Themes

--- themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/parsing/Series.kt
+++ themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/parsing/Series.kt
@@ -18,14 +18,40 @@ You should have received a copy of the GNU General Public License
 along with themes.moe-dl.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
+/**
+ * Series is a class that models an anime Series on [themes.moe](https://themes.moe-dl)
+ *
+ * It consists of a name, which is displayed on [themes.moe](https://themes.moe-dl),
+ * and a [List] of [Theme] objects attributed to this series.
+ *
+ * @param name The name of the series
+ * @param themes The [List] of [Theme] objects attributed to this series
+ */
 class Series constructor(val name: String, val themes: List<Theme>) {
 
-    fun download(target: String) {
+    /**
+     * Downloads all theme songs of this series to a specified directory
+     * Optionally converts the file into different formats
+     *
+     * @param target The target directory in which to save the files
+     * @param fileTypes The fileTypes to convert the file into. Defaults to only .webm
+     */
+    fun download(target: String, fileTypes: Array<FileTypes> = arrayOf(FileTypes.WEBM)) {
         for (theme in this.themes) {
-            theme.download(target)
+            theme.download(target, fileTypes)
         }
     }
 
+    /**
+     * Converts the [Series] object into a string for printing to the console
+     *
+     * The string is formatted like this:
+     *
+     *     Name: [Theme1, Theme2, Theme3, ...]
+     *
+     * @return the formatted [String]
+     */
     override fun toString() : String {
         return "${this.name}: ${this.themes}"
     }
--- themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/parsing/Theme.kt
+++ themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/parsing/Theme.kt
@@ -1,4 +1,7 @@
 package net.namibsun.themes_moe_dl.lib.parsing
+
+import java.nio.file.Paths
+
 /*
 Copyright Hermann Krumrey<hermann@krumreyh.com>, 2017
 
@@ -18,15 +21,65 @@ You should have received a copy of the GNU General Public License
 along with themes.moe-dl.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-class Theme constructor(description: String, url: String) {
-    val description = description
-    val url = url
+/**
+ * The Theme class models an anime theme song indexed on [themes.moe](https://themes.moe).
+ *
+ * It consists of a short description as displayed on [themes.moe](https://themes.moe),
+ * as well as a URL to the video file of the theme song
+ *
+ * @param description The description of the theme song
+ * @param url The URL to the theme song file
+ */
+class Theme constructor(val description: String, val url: String) {
 
-    fun download(target: String) {
+    /**
+     * Downloads the theme song file to the specified target directory using an automatic naming scheme
+     *
+     * The description is used as the file name, a suffix and prefix may also be supplied
+     * The downloader can automatically convert the file into different file types. By default,
+     * only the original .webm file is kept
+     *
+     * This method only generates a filename based off the information provided, then delegates
+     * the file download and conversion to the other [download] method
+     *
+     * @param targetDir The target directory in which the file should be saved
+     * @param fileTypes The filetypes to convert the file into. Defaults to only .webm
+     * @param prefix An optional prefix for the generated file name
+     * @param suffix An optional suffix for the generated file name
+     */
+    fun download(
+            targetDir: String,
+            fileTypes: Array<FileTypes> = arrayOf(FileTypes.WEBM),
+            prefix: String = "",
+            suffix: String = "") {
 
+        val filepath = Paths.get(targetDir, "$prefix${this.description}$suffix").toString()
+        this.download(filepath, fileTypes)
     }
 
+    /**
+     * Downloads the theme song file to a specified file name.
+     *
+     * The [fileTypes] parameter specifies which media formats the file should be
+     * converted to. By default, only the original .webm file is downloaded and left as-is.
+     *
+     * @param targetFile: The file name of the target file, without any file type associated suffixes like .webm etc.
+     * @param fileTypes: The types of media files to convert the theme song into once downloaded
+     */
+    fun download(targetFile: String, fileTypes: Array<FileTypes> = arrayOf(FileTypes.WEBM)) {
+        println(targetFile)
+    }
+
+    /**
+     * Formats the [Theme] object for printing to the console
+     *
+     * This is done in the following format:
+     *
+     *     Description: URL
+     *
+     * @return The formatted String
+     */
     override fun toString(): String {
-        return "${this.description}:${this.url}"
+        return "${this.description}: ${this.url}"
     }
 }
