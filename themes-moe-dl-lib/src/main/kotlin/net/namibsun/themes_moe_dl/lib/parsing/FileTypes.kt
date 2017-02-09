bc39106471b82f2652dd88b09280efb8c7dd6f33
Hermann Krumrey <hermann@krumreyh.com>
2017-02-09 10:58:29 +0100

    Implement basic models of Series and Themes
    Former-commit-id: c16bb7e578db8ef2e2ea286feda165f7714a94e3

---
+++ themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/parsing/FileTypes.kt
@@ -0,0 +1,31 @@
+package net.namibsun.themes_moe_dl.lib.parsing
+/*
+Copyright Hermann Krumrey<hermann@krumreyh.com>, 2017
+
+This file is part of themes.moe-dl.
+
+themes.moe-dl is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+themes.moe-dl is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with themes.moe-dl.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * An enum that defines which file types are currently supported for downloading
+ * from [themes.moe](https://themes.moe)
+ *
+ * Currently supported are:
+ *
+ * * .webm
+ */
+enum class FileTypes {
+    WEBM
+}
