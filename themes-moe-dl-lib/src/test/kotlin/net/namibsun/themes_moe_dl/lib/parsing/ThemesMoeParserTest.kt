df0c1eef4977e1906fc27b72463570733a10c1f3
Hermann Krumrey <hermann@krumreyh.com>
2017-02-10 17:37:43 +0100

    Write unit tests for easy parts of Parser

--- themes-moe-dl-lib/src/test/kotlin/net/namibsun/themes_moe_dl/lib/parsing/ThemesMoeParserTest.kt
+++ themes-moe-dl-lib/src/test/kotlin/net/namibsun/themes_moe_dl/lib/parsing/ThemesMoeParserTest.kt
@@ -40,32 +40,52 @@ class ThemesMoeParserTest {
         this.validateResults(results, arrayOf("91 Days"))
     }
 
-    /*
+    /**
+     * Tests if the parser correctly fetches the seasonal anime lists
+     *
+     * While doing so, the anime list of Winter 2017 is parsed and checked for the inclusion of a couple of anime
+     * series, while two further tests are conducted to make sure that the parser does not fail on
+     * seasons that are too far in the past or in the future, but rather return an empty list
+     */
     @Test
     fun testFetchingSeasonalList() {
-        val results = this.standardParser.fetchSeasonList(2016, Seasons.WINTER)
-        validateResults(results, arrayOf())
+
+        val results = this.standardParser.fetchSeasonList(2017, Seasons.WINTER)
+        validateResults(results, arrayOf("Youjo Senki", "Gintama.", "Little Witch Academia"))
+
+        assertTrue(this.standardParser.fetchSeasonList(1900, Seasons.SUMMER).isEmpty()) // Season too early in past
+        assertTrue(this.standardParser.fetchSeasonList(3000, Seasons.FALL).isEmpty()) // Season in future
     }
 
+    /**
+     * Tests fetching the popular list.
+     */
+    @Test
+    fun testFetchingPopularList() {
+        val results = this.standardParser.fetchPopularList()
+        validateResults(results, arrayOf("Sword Art Online", "Steins;Gate", "Shingeki no Kyojin"))
+    }
+
+    /*
     @Test
     fun testFetchingSearchResult() {
-        val results = this.standardParser.search("d-frag")
+        val results = this.standardParser.search("steins gate")
         validateResults(results, arrayOf("D-Frag!"))
     }
 
     @Test
-    fun testFetchingPopularList() {
-        val results = this.standardParser.fetchPopularList()
-        validateResults(results, arrayOf())
-    }
-
-    @Test
     fun testFetchingPlaylist() {
-        val results = this.standardParser.fetchPlayList(12345)
-        validateResults(results, arrayOf())
+        val results = this.standardParser.fetchPlayList(327031)
+        validateResults(results, arrayOf("Sword Art Online", "Steins;Gate", "Shingeki no Kyojin"))
     }
     */
 
+    /**
+     * Checks if a parse result is not empty and contains series with specified names
+     *
+     * @param results The parse result
+     * @param seriesNames The names that should be in the result
+     */
     fun validateResults(results: List<Series>, seriesNames: Array<String>) {
         assertTrue(results.isNotEmpty())
         for (seriesName in seriesNames) {
