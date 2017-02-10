eeda698ecde917ca077c699fefb698e560f552b3
Hermann Krumrey <hermann@krumreyh.com>
2017-02-10 08:49:50 +0100

    Remove private variable access from main

--- themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/Main.kt
+++ themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/Main.kt
@@ -1,7 +1,6 @@
 package net.namibsun.themes_moe_dl.lib
 
 import net.namibsun.themes_moe_dl.lib.parsing.ListTypes
-import net.namibsun.themes_moe_dl.lib.parsing.Theme
 import net.namibsun.themes_moe_dl.lib.parsing.ThemesMoeParser
 /**
  * @param args The
@@ -9,8 +8,6 @@ import net.namibsun.themes_moe_dl.lib.parsing.ThemesMoeParser
 fun main(args: Array<String>) {
 
     val userSeries = ThemesMoeParser().fetchUserList(args[0], ListTypes.MYANIMELIST)
-    println(Theme("A", "B").description)
-    return
     for (series in userSeries) {
         series.download(args[1])
     }
