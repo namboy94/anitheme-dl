ff7149824d38d0342f1b8ad83aadb5e07875037c
Hermann Krumrey <hermann@krumreyh.com>
2017-02-10 08:45:49 +0100

    Make instance variables private
    Former-commit-id: c8bfb42db9bf2b2680fe148b4913f4616d10f626

--- themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/Main.kt
+++ themes-moe-dl-lib/src/main/kotlin/net/namibsun/themes_moe_dl/lib/Main.kt
@@ -1,6 +1,7 @@
 package net.namibsun.themes_moe_dl.lib
 
 import net.namibsun.themes_moe_dl.lib.parsing.ListTypes
+import net.namibsun.themes_moe_dl.lib.parsing.Theme
 import net.namibsun.themes_moe_dl.lib.parsing.ThemesMoeParser
 /**
  * @param args The
@@ -8,6 +9,8 @@ import net.namibsun.themes_moe_dl.lib.parsing.ThemesMoeParser
 fun main(args: Array<String>) {
 
     val userSeries = ThemesMoeParser().fetchUserList(args[0], ListTypes.MYANIMELIST)
+    println(Theme("A", "B").description)
+    return
     for (series in userSeries) {
         series.download(args[1])
     }
