f5448f334dbfcf5b26dc6ba9c0e9335a48a3ffc9
Hermann Krumrey <hermann@krumreyh.com>
2017-02-11 14:24:50 +0100

    Create basic CLI parser that enables a large part of the library's functionality

--- themes-moe-dl-cli/src/main/kotlin/net/namibsun/themes_moe_dl/cli/Main.kt
+++ themes-moe-dl-cli/src/main/kotlin/net/namibsun/themes_moe_dl/cli/Main.kt
@@ -18,11 +18,16 @@ You should have received a copy of the GNU General Public License
 along with themes.moe-dl.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+import net.namibsun.themes_moe_dl.lib.parsing.ListTypes
+import net.namibsun.themes_moe_dl.lib.parsing.Seasons
 import net.namibsun.themes_moe_dl.lib.parsing.Series
 import net.namibsun.themes_moe_dl.lib.parsing.ThemesMoeParser
-import org.apache.commons.cli.*
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.GnuParser
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.UnrecognizedOptionException
 import kotlin.system.exitProcess
-
 
 fun main(args: Array<String>) {
 
@@ -35,10 +40,22 @@ fun parseArgs(args: Array<String>): CommandLine {
 
     val options = Options()
     options.addOption("h", "help", false, "Prints this help message")
-    options.addOption("u", "user", false, "Specifies the myanimelist.net user when downloading the user's theme song list ")
     options.addOption("o", "no-ops", false, "Disables Openings")
     options.addOption("e", "no-eds", false, "Disables Endings")
-    options.addOption("m", "mode", true, "The mode to run the downloader in. Currently available: popular")
+    options.addOption("m", "mode", true, "The mode to run the downloader in. Currently available:\n" +
+            "- popular:      Downloads all themes from the popular list on themes.moe\n" +
+            "- mal-userlist: Downloads all themes associated with a user's MyAnimeList account\n" +
+            "- seasonal:     Downloads all themes in a specific season\n" +
+            "- search:       Downloads all results of a search query\n" +
+            "- playlist:     Downloads all themes from a specific playlist")
+    options.addOption("d", "destination", true, "Specifies a destination directory for the downloaded files")
+    options.addOption("u", "mal-username", true, "Specifies the myanimelist.net user in the 'mal-userlist' mode ")
+    options.addOption("y", "year", true, "Specifies the year in the 'seasonal' mode")
+    options.addOption("s", "season", true, "Specifies the season in the 'seasonal' mode.\n" +
+            "Must be one of the following:\n" +
+            "- SUMMER\n- WINTER\n- FALL\n- SPRING")
+    options.addOption("i", "playlist-id", true, "specifies the playlist ID in the 'playlist' mode")
+    options.addOption("t", "term", true, "specifies the search term in the 'search' mode")
 
     val parser = GnuParser()
 
@@ -69,18 +86,66 @@ fun generateThemesMoeparserFromOptions(options: CommandLine): ThemesMoeParser {
 
 fun executeThemesMoeParser(options: CommandLine, themesMoeParser: ThemesMoeParser) {
 
-    val results: List<Series>
-    if(options.getOptionValue("mode") == "popular") {
-        results = themesMoeParser.fetchPopularList()
-    }
-    else {
-        println("No valid mode passed. See --help for more information")
-        exitProcess(0)
+    var results: List<Series> = listOf()
+
+    when (options.getOptionValue("mode")) {
+        "popular" -> results = themesMoeParser.fetchPopularList()
+        "mal-userlist" -> {
+            if (!options.hasOption("mal-username")) {
+                println("The myanimelist.net username is required. Pass it with the --mal-username option")
+            }
+            else {
+                results = themesMoeParser.fetchUserList(options.getOptionValue("mal-username"), ListTypes.MYANIMELIST)
+            }
+        }
+        "seasonal" -> {
+            if (!options.hasOption("season") || !options.hasOption("year")) {
+                println("Seasonal lists require a season and a year passed using --year and --season")
+            }
+            else {
+                try {
+                    val year = options.getOptionValue("year").toInt()
+                    val season = Seasons.valueOf(options.getOptionValue("season").toUpperCase())
+                    results = themesMoeParser.fetchSeasonList(year, season)
+                } catch (e: NumberFormatException) {
+                    println("The year must be a whole number")
+                } catch (e: IllegalArgumentException) {
+                    println("${options.getOptionValue("season")} is not a valid season")
+                }
+            }
+        }
+        "playlist" -> {
+            if (!options.hasOption("playlist-id")) {
+                println("A playlist ID must be specified with --playlist-id")
+            }
+            else {
+                try {
+                    val playlist = options.getOptionValue("playlist-id").toInt()
+                    results = themesMoeParser.fetchPlayList(playlist)
+                } catch (e: NumberFormatException) {
+                    println("The playlist ID must be an Integer value")
+                }
+            }
+        }
+        "search" -> {
+            if (!options.hasOption("term")) {
+                println("The search term must be provided using --term")
+            }
+            else {
+                results = themesMoeParser.search(options.getOptionValue("term"))
+            }
+        }
+        else -> {
+            println("No valid mode passed. See --help for more information")
+        }
     }
 
     for (result in results) {
-        result.download("here")
+        if (options.hasOption("destination")) {
+            result.download(options.getOptionValue("destination"))
+        }
+        else {
+            result.download("themes-moe")
+        }
     }
-
-
 }
