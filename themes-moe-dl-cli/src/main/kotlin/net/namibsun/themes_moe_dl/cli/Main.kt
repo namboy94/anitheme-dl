c529753d5c71f4b169ee3dcd8c5e18cb552f1cf5
Hermann Krumrey <hermann@krumreyh.com>
2017-02-11 13:01:59 +0100

    First working implementation of the CLI
    Former-commit-id: b6633d46e1082d4981f8f631d23a5cd4b7ecad1a

--- themes-moe-dl-cli/src/main/kotlin/net/namibsun/themes_moe_dl/cli/Main.kt
+++ themes-moe-dl-cli/src/main/kotlin/net/namibsun/themes_moe_dl/cli/Main.kt
@@ -18,20 +18,69 @@ You should have received a copy of the GNU General Public License
 along with themes.moe-dl.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+import net.namibsun.themes_moe_dl.lib.parsing.Series
 import net.namibsun.themes_moe_dl.lib.parsing.ThemesMoeParser
-import org.apache.commons.cli.GnuParser
-import org.apache.commons.cli.Options
+import org.apache.commons.cli.*
+import kotlin.system.exitProcess
+
 
 fun main(args: Array<String>) {
 
+    val options = parseArgs(args)
+    val themesMoeParser = generateThemesMoeparserFromOptions(options)
+    executeThemesMoeParser(options, themesMoeParser)
+}
+
+fun parseArgs(args: Array<String>): CommandLine {
+
     val options = Options()
-    options.addOption("a", false, "Test")
+    options.addOption("h", "help", false, "Prints this help message")
+    options.addOption("u", "user", false, "Specifies the myanimelist.net user when downloading the user's theme song list ")
+    options.addOption("o", "no-ops", false, "Disables Openings")
+    options.addOption("e", "no-eds", false, "Disables Endings")
+    options.addOption("m", "mode", true, "The mode to run the downloader in. Currently available: popular")
 
-    val commandLineParser = GnuParser()
-    val results = commandLineParser.parse(options, args)
+    val parser = GnuParser()
 
-    println(results.hasOption("a"))
+    try {
 
-    println("Hello World")
-    println(ThemesMoeParser().fetchPopularList())
+        val results = parser.parse(options, args)
+
+        if (results.hasOption("help")) {
+            val formatter = HelpFormatter()
+            formatter.printHelp("themes.moe Downloader CLI", options)
+        }
+        else if (!results.hasOption("mode")) {
+            println("Option 'mode' is required. Use --help for more information")
+            exitProcess(0)
+        }
+
+        return results
+
+    } catch (e: UnrecognizedOptionException) {
+        println(e.message)
+        exitProcess(0)
+    }
+}
+
+fun generateThemesMoeparserFromOptions(options: CommandLine): ThemesMoeParser {
+    return ThemesMoeParser(includeOp = !options.hasOption("no-ops"), includeEd = !options.hasOption("no-eds"))
+}
+
+fun executeThemesMoeParser(options: CommandLine, themesMoeParser: ThemesMoeParser) {
+
+    val results: List<Series>
+    if(options.getOptionValue("mode") == "popular") {
+        results = themesMoeParser.fetchPopularList()
+    }
+    else {
+        println("No valid mode passed. See --help for more information")
+        exitProcess(0)
+    }
+
+    for (result in results) {
+        result.download("here")
+    }
+
+
 }
